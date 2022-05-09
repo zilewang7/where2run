8186fdca8bcd8057ac9902d98ba1cf1ac1be9fc6
DDC <643063583@qq.com>
2022-05-09 14:42:06 +0800

    sideMenu&Carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { Row, Col } from 'antd';
 import React from 'react';
 import styles from './App.module.css';
-import { Header, Footer } from './components'
+import { Header, Footer, SideMenu, Carousel } from './components'
 
 function App() {
   return (
     <div className={styles.App}>
       <Header />
+      <div className={styles['page-content']}>
+        <Row style={{ marginTop: 20 }}>
+          <Col span={6}>
+            <SideMenu />
+          </Col>
+          <Col span={18}>
+            <Carousel />
+          </Col>
+        </Row>
+      </div>
       <Footer />
     </div >
   );
