8a26fa30988ce0e95ad88778dd230d047f15aac3
DDC <643063583@qq.com>
2022-05-11 22:31:02 +0800

    react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,20 @@
-import { Row, Col, Typography } from 'antd';
 import React from 'react';
 import styles from './App.module.css';
-import { Header, Footer, SideMenu, Carousel, ProductCollection, CovidMap, } from './components';
-import { productList1, productList2, productList3 } from './mockups';
-
-import sideImage from './assets/images/sider_2019_12-09.png';
-import sideImage2 from './assets/images/sider_2019_02-04.png';
-import sideImage3 from './assets/images/sider_2019_02-04-2.png';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages";
 
 function App() {
   return (
     <div className={styles.App}>
-      <Header />
-      <main className={styles['page-content']}>
-        <Row style={{ marginTop: 20 }}>
-          <Col span={6}>
-            <SideMenu />
-          </Col>
-          <Col span={18}>
-            <Carousel />
-          </Col>
-        </Row>
-        <ProductCollection
-          title={<Typography.Title level={3} type="warning">推荐</Typography.Title>}
-          sideImage={sideImage}
-          products={productList1}
-        />
-        <ProductCollection
-          title={<Typography.Title level={3} type="danger">新品</Typography.Title>}
-          sideImage={sideImage2}
-          products={productList2}
-        /><ProductCollection
-          title={<Typography.Title level={3} type="success">国内</Typography.Title>}
-          sideImage={sideImage3}
-          products={productList3}
-        />
-      </main>
-      <CovidMap />
-      <Footer />
+      {/* <BrowserRouter>
+        <Route path='/' element={<HomePage />} />
+      </BrowserRouter> */}
+      {/* <HomePage /> */}
+      <Routes>
+        <Route path="/" element={<HomePage />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
     </div >
   );
 }
---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Row, Col, Typography } from 'antd';
+import { Header, Footer, SideMenu, Carousel, ProductCollection, CovidMap, } from '../../components';
+import { productList1, productList2, productList3 } from '../../pages/home/mockups';
+
+import sideImage from '../../assets/images/sider_2019_12-09.png';
+import sideImage2 from '../../assets/images/sider_2019_02-04.png';
+import sideImage3 from '../../assets/images/sider_2019_02-04-2.png';
+
+import styles from './HomePage.module.css';
+
+
+export class HomePage extends React.Component {
+    render(): React.ReactNode {
+        return (
+            <>
+                <Header />
+                <main className={styles['page-content']}>
+                    <Row style={{ marginTop: 20 }}>
+                        <Col span={6}>
+                            <SideMenu />
+                        </Col>
+                        <Col span={18}>
+                            <Carousel />
+                        </Col>
+                    </Row>
+                    <ProductCollection
+                        title={<Typography.Title level={3} type="warning">推荐</Typography.Title>}
+                        sideImage={sideImage}
+                        products={productList1}
+                    />
+                    <ProductCollection
+                        title={<Typography.Title level={3} type="danger">新品</Typography.Title>}
+                        sideImage={sideImage2}
+                        products={productList2}
+                    /><ProductCollection
+                        title={<Typography.Title level={3} type="success">国内</Typography.Title>}
+                        sideImage={sideImage3}
+                        products={productList3}
+                    />
+                </main>
+                <CovidMap />
+                <Footer />
+            </>
+        );
+    }
+}
